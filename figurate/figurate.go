f7f516ccfe6ca01924e5fce925deaad42001c8ca
Erik Bryant <erikbryantology@gmail.com>
2024-01-07 10:42:19 -0500

    Change references from util package to algebra package

--- figurate/figurate.go
+++ figurate/figurate.go
@@ -6,26 +6,26 @@ package figurate
 import (
 	"math"
 
-	"github.com/erikbryant/util-golang/util"
+	"github.com/erikbryant/util-golang/algebra"
 )
 
 // IsTriangular returns true if n is a triangular number
 func IsTriangular(n int) bool {
 	// https://en.wikipedia.org/wiki/Triangular_number
 	root := math.Sqrt(float64(8*n + 1))
-	return util.IsInt(root)
+	return algebra.IsInt(root)
 }
 
 // IsPentagonal returns true if n is a pentagonal number
 func IsPentagonal(n int) bool {
 	// https://en.wikipedia.org/wiki/Pentagonal_number
 	root := math.Sqrt(float64(24*n + 1))
-	return util.IsInt((root + 1) / 6)
+	return algebra.IsInt((root + 1) / 6)
 }
 
 // IsHexagonal returns true if n is a hexagonal number
 func IsHexagonal(n int) bool {
 	// https://en.wikipedia.org/wiki/Hexagonal_number
 	root := math.Sqrt(float64(8*n + 1))
-	return util.IsInt((root + 1) / 4)
+	return algebra.IsInt((root + 1) / 4)
 }
