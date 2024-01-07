8e5dd153934a9ee59e6ee71fab9cf0cf82bcc3cd
Erik Bryant <erikbryantology@gmail.com>
2024-01-07 10:41:03 -0500

    Add algebraic functions

--- algebra/algebra.go
+++ algebra/algebra.go
@@ -2,6 +2,9 @@ package algebra
 
 import (
 	"math"
+	"math/big"
+
+	"github.com/erikbryant/util-golang/primes"
 )
 
 // NextSquare returns the next highest square and its square root
@@ -17,4 +20,359 @@ func NextSquare(n uint) (uint, uint) {
 
 	next := root*root + root<<1 + 1
 	return next, root + 1
+}
+
+type convergentSeries func(int) int64
+
+// E returns the nth number (1-based) in the convergent series of e
+func E(n int) int64 {
+	// e.g., [2; 1,2,1, 1,4,1, 1,6,1, ... ,1,2k,1, ...]
+	if n == 1 {
+		return int64(2)
+	}
+	if n%3 == 0 {
+		return int64(2 * n / 3)
+	}
+	return int64(1)
+}
+
+// Sqrt2 returns the nth number (1-based) in the convergent series of root 2
+func Sqrt2(n int) int64 {
+	// e.g., [2;(2)]
+
+	if n == 1 {
+		return int64(1)
+	}
+	return int64(2)
+}
+
+// Convergent returns the nth convegence of whichever series you pass in a function for
+func Convergent(n int, fn convergentSeries) (*big.Int, *big.Int) {
+	numerator := big.NewInt(fn(n))
+	denominator := big.NewInt(1)
+
+	for n > 1 {
+		// Invert
+		denominator, numerator = numerator, denominator
+
+		// Add e(n-1)
+		product := big.NewInt(fn(n - 1))
+		product.Mul(product, denominator)
+		numerator.Add(numerator, product)
+
+		n--
+	}
+
+	return numerator, denominator
+}
+
+// Divisors returns a sorted list of all positive divisors of n
+func Divisors(n int) []int {
+	// Everything is divisible by 1
+	d := []int{1}
+
+	root := int(math.Sqrt(float64(n)))
+
+	// Degenerate cases
+	if root <= 1 {
+		if n < 0 {
+			return []int{}
+		}
+		if n == 0 || n == 1 {
+			return d
+		}
+		d = append(d, n)
+		return d
+	}
+
+	// Find the lower divisors
+	for i := 2; i < root; i++ {
+		if n%i == 0 {
+			d = append(d, i)
+		}
+	}
+
+	// Check for the special case of n being a perfect square
+	var start int
+	if root*root == n {
+		d = append(d, root)
+		start = len(d) - 2
+	} else {
+		if n%root == 0 {
+			d = append(d, root)
+		}
+		start = len(d) - 1
+	}
+
+	// Add the upper divisors (the inverses of the lower divisors)
+	for i := start; i >= 0; i-- {
+		d = append(d, n/d[i])
+	}
+
+	return d
+}
+
+// Factors returns a sorted list of the unique prime factors of n (excluding n)
+func Factors(n int) []int {
+	if primes.Prime(n) {
+		return []int{}
+	}
+
+	f := []int{}
+
+	for i := 0; primes.PackedPrimes[i] <= n; i++ {
+		if n%primes.PackedPrimes[i] == 0 {
+			f = append(f, primes.PackedPrimes[i])
+			n /= primes.PackedPrimes[i]
+		}
+	}
+
+	return f
+}
+
+// FactorsCounted returns counts of how many times each factor divides into n
+func FactorsCounted(n int) map[int]int {
+	factors := make(map[int]int)
+
+	// Find all of the 2 factors, since they are quick
+	for (n & 0x01) == 0 {
+		factors[2]++
+		n = n >> 1
+		if n == 1 {
+			return factors
+		}
+	}
+
+	root := int(math.Sqrt(float64(n)))
+	for i := 1; primes.PackedPrimes[i] <= root; i++ {
+		p := primes.PackedPrimes[i]
+		for n%p == 0 {
+			factors[p]++
+			n = n / p
+			if n == 1 {
+				return factors
+			}
+		}
+	}
+
+	// We did not find any factors for 'n',
+	// so it must be prime.
+	factors[n]++
+
+	return factors
+}
+
+// GCD returns the greatest common divisor of a and b
+func GCD(a, b int) int {
+	// https://en.wikipedia.org/wiki/Greatest_common_divisor
+
+	if a == 0 && b == 0 {
+		return 0
+	}
+
+	for a > 0 && b > 0 {
+		a, b = min(a, b), max(a, b)%min(a, b)
+	}
+
+	return max(a, b)
+}
+
+// LCM returns the least common multiple of a and b
+func LCM(a, b int) int {
+	// https://en.wikipedia.org/wiki/Least_common_multiple
+
+	if a == 0 && b == 0 {
+		return 0
+	}
+
+	return (a / GCD(a, b)) * b
+}
+
+func ReduceFraction(n, d int) (int, int) {
+	gcd := GCD(n, d)
+	return n / gcd, d / gcd
+}
+
+// SumFraction returns the sum of the two fractions, still in fraction form
+func SumFraction(n1, d1, n2, d2 int) (int, int) {
+	lcm := LCM(d1, d2)
+	n1Scalar := lcm / d1
+	n2Scalar := lcm / d2
+	return (n1 * n1Scalar) + (n2 * n2Scalar), lcm
+}
+
+// MulFraction returns the product of the two fractions, still in fraction form
+func MulFraction(n1, d1, n2, d2 int) (int, int) {
+	n1, d1 = ReduceFraction(n1, d1)
+	n2, d2 = ReduceFraction(n2, d2)
+	a, b := ReduceFraction(n1*n2, d1*d2)
+	return a, b
+}
+
+// IsInt returns true if n is an integer
+func IsInt(n float64) bool {
+	return n == float64(int(n))
+}
+
+// IsSquare returns true if n is a square number
+func IsSquare(n int) bool {
+	root := math.Sqrt(float64(n))
+	return IsInt(root)
+}
+
+// IsCube returns true if n is a cube number
+func IsCube(n int) bool {
+	root := math.Cbrt(float64(n))
+	return IsInt(root)
+}
+
+// EqualIntSlice returns true if the two slices have identical contents
+func EqualIntSlice(a, b []int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+// Reverse reverses the order of the elements in a slice
+func Reverse(digits []int) []int {
+	rev := make([]int, 0)
+
+	for i := len(digits) - 1; i >= 0; i-- {
+		rev = append(rev, digits[i])
+	}
+
+	return rev
+}
+
+// IntToDigits converts an int into a slice of its component digits
+func IntToDigits(n int) []int {
+	digits := make([]int, 0)
+
+	for n > 0 {
+		d := n % 10
+		digits = append(digits, d)
+		n = n / 10
+	}
+
+	return Reverse(digits)
+}
+
+// DigitsToInt converts a slice of digits to an int
+func DigitsToInt(digits []int) int {
+	number := 0
+
+	for i := 0; i < len(digits); i++ {
+		number += digits[i] * int(math.Pow(10.0, float64(len(digits)-1-i)))
+	}
+
+	return number
+}
+
+// DigitSum returns the sum of the digits in the number
+func DigitSum(n int) int {
+	sum := 0
+
+	for n > 0 {
+		sum += n % 10
+		n /= 10
+	}
+
+	return sum
+}
+
+// Harshad returns true if n is divisible by the sum of its digits
+func Harshad(n, sum int) bool {
+	return n%sum == 0
+}
+
+// Totient returns how many numbers k are relatively prime to n
+func Totient(n int) int {
+	// ... where  1 <= k < n. Relatively prime means that they have
+	// no common divisors (other than 1). 1 is considered relatively
+	// prime to all other numbers.
+	//
+	// From https://www.doc.ic.ac.uk/~mrh/330tutor/ch05s02.html
+	//
+	// The general formula to compute φ(n) is the following:
+	//
+	// If the prime factorisation of n is given by n =p1e1*...*pnen, then
+	// φ(n) = n *(1 - 1/p1)* ... (1 - 1/pn).
+	//
+	// For example:
+	//
+	// 9 = 32, φ(9) = 9* (1-1/3) = 6
+	//
+	// 4 =22, φ(4) = 4* (1-1/2) = 2
+	//
+	// 15 = 3*5, φ(15) = 15* (1-1/3)*(1-1/5) = 15*(2/3)*(4/5) =8
+
+	if primes.Prime(n) {
+		return n - 1
+	}
+
+	factors := Factors(n)
+	count := n
+
+	for _, f := range factors {
+		count /= f
+		count *= (f - 1)
+	}
+
+	return count
+}
+
+// SquareFree returns true if no square of a prime divides n
+func SquareFree(n int) bool {
+	for _, prime := range primes.PackedPrimes {
+		if prime > int(math.Sqrt(float64(n))) {
+			break
+		}
+
+		if n%(prime*prime) == 0 {
+			return false
+		}
+	}
+
+	return true
+}
+
+// PascalTriangle returns a triangle of the max depth specified
+func PascalTriangle(max int) [][]int {
+	// We build the triangle left-justified. A cell is the sum of the cell above it
+	// and the cell above and to the left.
+	//
+	//	1: 1
+	//	2: 1 1
+	//	3: 1 2 1
+	//	4: 1 3 3 1
+	//	5: 1 4 6 4 1
+
+	rows := [][]int{}
+	var row []int
+
+	// Create the empty rows
+	for i := 0; i < max; i++ {
+		row = make([]int, i+1)
+		rows = append(rows, row)
+	}
+
+	for i := 0; i < max; i++ {
+		for j := range rows[i] {
+			if j == 0 || j == len(rows[i])-1 {
+				rows[i][j] = 1
+				continue
+			}
+			rows[i][j] = rows[i-1][j] + rows[i-1][j-1]
+		}
+	}
+
+	return rows
 }
--- algebra/algebra_test.go
+++ algebra/algebra_test.go
@@ -1,6 +1,9 @@
 package algebra
 
-import "testing"
+import (
+	"math/big"
+	"testing"
+)
 
 func TestNextSquare(t *testing.T) {
 	testCases := []struct {
@@ -21,6 +24,572 @@ func TestNextSquare(t *testing.T) {
 		answer, answer2 := NextSquare(testCase.c)
 		if answer != testCase.expected || answer2 != testCase.expected2 {
 			t.Errorf("ERROR: For %d expected %d/%d, got %d/%d", testCase.c, testCase.expected, testCase.expected2, answer, answer2)
+		}
+	}
+}
+
+func TestE(t *testing.T) {
+	testCases := []struct {
+		n        int
+		expected int64
+	}{
+		{1, 2},
+		{2, 1},
+		{3, 2},
+		{4, 1},
+		{5, 1},
+		{6, 4},
+		{7, 1},
+		{8, 1},
+		{9, 6},
+	}
+
+	for _, testCase := range testCases {
+		answer := E(testCase.n)
+		if answer != testCase.expected {
+			t.Errorf("ERROR: For %d expected %d, got %d", testCase.n, testCase.expected, answer)
+		}
+	}
+}
+
+func TestConvergentE(t *testing.T) {
+	testCases := []struct {
+		n         int
+		expectedN int64
+		expectedD int64
+	}{
+		{1, 2, 1},
+		{2, 3, 1},
+		{3, 8, 3},
+		{4, 11, 4},
+		{5, 19, 7},
+		{6, 87, 32},
+		{7, 106, 39},
+		{8, 193, 71},
+		{9, 1264, 465},
+		{10, 1457, 536},
+	}
+
+	for _, testCase := range testCases {
+		expectedN := big.NewInt(testCase.expectedN)
+		expectedD := big.NewInt(testCase.expectedD)
+		answerN, answerD := Convergent(testCase.n, E)
+		if answerN.Cmp(expectedN) != 0 || answerD.Cmp(expectedD) != 0 {
+			t.Errorf("ERROR: For %d expected %d/%d, got %d/%d", testCase.n, testCase.expectedN, testCase.expectedD, answerN, answerD)
+		}
+	}
+}
+
+func TestConvergentSqrt2(t *testing.T) {
+	testCases := []struct {
+		n         int
+		expectedN int64
+		expectedD int64
+	}{
+		{1, 1, 1},
+		{2, 3, 2},
+		{3, 7, 5},
+		{4, 17, 12},
+		{5, 41, 29},
+		{6, 99, 70},
+		{7, 239, 169},
+		{8, 577, 408},
+	}
+
+	for _, testCase := range testCases {
+		expectedN := big.NewInt(testCase.expectedN)
+		expectedD := big.NewInt(testCase.expectedD)
+		answerN, answerD := Convergent(testCase.n, Sqrt2)
+		if answerN.Cmp(expectedN) != 0 || answerD.Cmp(expectedD) != 0 {
+			t.Errorf("ERROR: For %d expected %d/%d, got %d/%d", testCase.n, testCase.expectedN, testCase.expectedD, answerN, answerD)
+		}
+	}
+}
+
+func TestDivisors(t *testing.T) {
+	testCases := []struct {
+		n        int
+		expected []int
+	}{
+		{0, []int{1}},
+		{1, []int{1}},
+		{2, []int{1, 2}},
+		{3, []int{1, 3}},
+		{4, []int{1, 2, 4}},
+		{5, []int{1, 5}},
+		{6, []int{1, 2, 3, 6}},
+		{7, []int{1, 7}},
+		{8, []int{1, 2, 4, 8}},
+		{9, []int{1, 3, 9}},
+		{10, []int{1, 2, 5, 10}},
+		{11, []int{1, 11}},
+		{12, []int{1, 2, 3, 4, 6, 12}},
+		{20, []int{1, 2, 4, 5, 10, 20}},
+		{28, []int{1, 2, 4, 7, 14, 28}},
+		{100, []int{1, 2, 4, 5, 10, 20, 25, 50, 100}},
+		{210, []int{1, 2, 3, 5, 6, 7, 10, 14, 15, 21, 30, 35, 42, 70, 105, 210}},
+		{2310, []int{1, 2, 3, 5, 6, 7, 10, 11, 14, 15, 21, 22, 30, 33, 35, 42, 55, 66, 70, 77, 105, 110, 154, 165, 210, 231, 330, 385, 462, 770, 1155, 2310}},
+	}
+
+	for _, testCase := range testCases {
+		answer := Divisors(testCase.n)
+		if !EqualIntSlice(answer, testCase.expected) {
+			t.Errorf("ERROR: For %d expected %v, got %v", testCase.n, testCase.expected, answer)
+		}
+	}
+}
+
+func TestFactors(t *testing.T) {
+	testCases := []struct {
+		n        int
+		expected []int
+	}{
+		{2, []int{}},
+		{3, []int{}},
+		{4, []int{2}},
+		{5, []int{}},
+		{6, []int{2, 3}},
+		{7, []int{}},
+		{8, []int{2}},
+		{9, []int{3}},
+		{10, []int{2, 5}},
+		{11, []int{}},
+		{12, []int{2, 3}},
+		{20, []int{2, 5}},
+		{28, []int{2, 7}},
+		{210, []int{2, 3, 5, 7}},
+		{2310, []int{2, 3, 5, 7, 11}},
+	}
+
+	for _, testCase := range testCases {
+		answer := Factors(testCase.n)
+		if !EqualIntSlice(answer, testCase.expected) {
+			t.Errorf("ERROR: For %d expected %v, got %v", testCase.n, testCase.expected, answer)
+		}
+	}
+}
+
+func TestFactorsCounted(t *testing.T) {
+	testCases := []struct {
+		n        int
+		expected map[int]int
+	}{
+		{2, map[int]int{2: 1}},
+		{3, map[int]int{3: 1}},
+		{4, map[int]int{2: 2}},
+		{5, map[int]int{5: 1}},
+		{6, map[int]int{2: 1, 3: 1}},
+		{7, map[int]int{7: 1}},
+		{8, map[int]int{2: 3}},
+		{9, map[int]int{3: 2}},
+		{10, map[int]int{2: 1, 5: 1}},
+		{11, map[int]int{11: 1}},
+		{12, map[int]int{2: 2, 3: 1}},
+		{28, map[int]int{2: 2, 7: 1}},
+		{210, map[int]int{2: 1, 3: 1, 5: 1, 7: 1}},
+		{2310, map[int]int{2: 1, 3: 1, 5: 1, 7: 1, 11: 1}},
+	}
+
+	for _, testCase := range testCases {
+		answer := FactorsCounted(testCase.n)
+		if len(answer) != len(testCase.expected) {
+			t.Errorf("ERROR: For %d expected len=%d, got len=%d %v", testCase.n, len(testCase.expected), len(answer), answer)
+		}
+		for key := range testCase.expected {
+			if answer[key] != testCase.expected[key] {
+				t.Errorf("ERROR: For %d expected %v, got %v", testCase.n, testCase.expected, answer)
+			}
+		}
+	}
+}
+
+func TestGCD(t *testing.T) {
+	testCases := []struct {
+		a, b     int
+		expected int
+	}{
+		{0, 0, 0},
+		{1, 0, 1},
+		{0, 1, 1},
+		{1, 2, 1},
+		{3, 5, 1},
+		{2, 4, 2},
+		{9, 28, 1},
+		{200, 100, 100},
+	}
+
+	for _, testCase := range testCases {
+		answer := GCD(testCase.a, testCase.b)
+		if answer != testCase.expected {
+			t.Errorf("ERROR: For %d, %d expected %d, got %d", testCase.a, testCase.b, testCase.expected, answer)
+		}
+	}
+}
+
+func TestLCM(t *testing.T) {
+	testCases := []struct {
+		a, b     int
+		expected int
+	}{
+		{0, 0, 0},
+		{1, 0, 0},
+		{0, 1, 0},
+		{1, 2, 2},
+		{2, 2, 2},
+		{2, 4, 4},
+	}
+
+	for _, testCase := range testCases {
+		answer := LCM(testCase.a, testCase.b)
+		if answer != testCase.expected {
+			t.Errorf("ERROR: For %d, %d expected %d, got %d", testCase.a, testCase.b, testCase.expected, answer)
+		}
+	}
+}
+
+func TestSumFraction(t *testing.T) {
+	testCases := []struct {
+		a, b      int
+		c, d      int
+		expectedN int
+		expectedD int
+	}{
+		// Equal denominators
+		{1, 2, 3, 2, 4, 2},
+		{2, 7, 5, 7, 7, 7},
+		// Differing denominators
+		{1, 2, 1, 3, 5, 6},
+		{1, 2, 3, 10, 8, 10},
+		{2, 30, 2, 72, 34, 360},
+	}
+
+	for _, testCase := range testCases {
+		answer, answer2 := SumFraction(testCase.a, testCase.b, testCase.c, testCase.d)
+		if answer != testCase.expectedN || answer2 != testCase.expectedD {
+			t.Errorf("ERROR: For %d/%d + %d/%d expected %d/%d, got %d/%d", testCase.a, testCase.b, testCase.c, testCase.d, testCase.expectedN, testCase.expectedD, answer, answer2)
+		}
+	}
+}
+
+func TestMulFraction(t *testing.T) {
+	testCases := []struct {
+		a, b      int
+		c, d      int
+		expectedN int
+		expectedD int
+	}{
+		{2, 2, 3, 3, 1, 1},
+		{1, 2, 3, 2, 3, 4},
+		{5, 7, 2, 8, 5, 28},
+		{6, 9, 1, 3, 2, 9},
+		{3, 4, 3, 12, 3, 16},
+		{2, 30, 2, 72, 1, 540},
+	}
+
+	for _, testCase := range testCases {
+		answer, answer2 := MulFraction(testCase.a, testCase.b, testCase.c, testCase.d)
+		if answer != testCase.expectedN || answer2 != testCase.expectedD {
+			t.Errorf("ERROR: For %d/%d * %d/%d expected %d/%d, got %d/%d", testCase.a, testCase.b, testCase.c, testCase.d, testCase.expectedN, testCase.expectedD, answer, answer2)
+		}
+	}
+}
+
+func TestIsInt(t *testing.T) {
+	testCases := []struct {
+		c        float64
+		expected bool
+	}{
+		{-3.000, true},
+		{-1.0, true},
+		{0.0, true},
+		{0.01, false},
+		{1.0, true},
+		{3.1415, false},
+		{9999.0, true},
+	}
+
+	for _, testCase := range testCases {
+		answer := IsInt(testCase.c)
+		if answer != testCase.expected {
+			t.Errorf("ERROR: For %f expected %t, got %t", testCase.c, testCase.expected, answer)
+		}
+	}
+}
+
+func TestIsSquare(t *testing.T) {
+	testCases := []struct {
+		c        int
+		expected bool
+	}{
+		{-4, false},
+		{0, true},
+		{1, true},
+		{2, false},
+		{4, true},
+		{1000000, true},
+	}
+
+	for _, testCase := range testCases {
+		answer := IsSquare(testCase.c)
+		if answer != testCase.expected {
+			t.Errorf("ERROR: For %d expected %t, got %t", testCase.c, testCase.expected, answer)
+		}
+	}
+}
+
+func TestIsCube(t *testing.T) {
+	testCases := []struct {
+		c        int
+		expected bool
+	}{
+		{-3, false},
+		{-1, true},
+		{0, true},
+		{1, true},
+		{2, false},
+		{8, true},
+		{125, true},
+		{27000, true},
+	}
+
+	for _, testCase := range testCases {
+		answer := IsCube(testCase.c)
+		if answer != testCase.expected {
+			t.Errorf("ERROR: For %d expected %t, got %t", testCase.c, testCase.expected, answer)
+		}
+	}
+}
+
+func TestReverse(t *testing.T) {
+	testCases := []struct {
+		n        []int
+		expected []int
+	}{
+		{[]int{5, 6, 0, 0, 3}, []int{3, 0, 0, 6, 5}},
+		{[]int{2}, []int{2}},
+		{[]int{2, 3}, []int{3, 2}},
+	}
+
+	for _, testCase := range testCases {
+		answer := Reverse(testCase.n)
+		if len(answer) != len(testCase.expected) {
+			t.Errorf("ERROR: For %v expected %v, got %v", testCase.n, testCase.expected, answer)
+		}
+		for i := 0; i < len(testCase.expected); i++ {
+			if answer[i] != testCase.expected[i] {
+				t.Errorf("ERROR: For %v expected %v, got %v", testCase.n, testCase.expected, answer)
+			}
+		}
+	}
+}
+
+func TestIntToDigits(t *testing.T) {
+	testCases := []struct {
+		n        int
+		expected []int
+	}{
+		{56003, []int{5, 6, 0, 0, 3}},
+		{2, []int{2}},
+		{23, []int{2, 3}},
+		{1230, []int{1, 2, 3, 0}},
+		// {0, []int{0}},  // Not implemented yet.
+	}
+
+	for _, testCase := range testCases {
+		answer := IntToDigits(testCase.n)
+		if len(answer) != len(testCase.expected) {
+			t.Errorf("ERROR: For %v expected %v, got %v", testCase.n, testCase.expected, answer)
+		}
+		for i := 0; i < len(testCase.expected); i++ {
+			if answer[i] != testCase.expected[i] {
+				t.Errorf("ERROR: For %v expected %v, got %v", testCase.n, testCase.expected, answer)
+			}
+		}
+	}
+}
+
+func TestDigitsToInt(t *testing.T) {
+	testCases := []struct {
+		n        []int
+		expected int
+	}{
+		{[]int{5, 6, 0, 0, 3}, 56003},
+		{[]int{2}, 2},
+		{[]int{2, 3}, 23},
+		{[]int{1, 2, 3, 0}, 1230},
+		{[]int{0}, 0},
+	}
+
+	for _, testCase := range testCases {
+		answer := DigitsToInt(testCase.n)
+		if answer != testCase.expected {
+			t.Errorf("ERROR: For %v expected %d, got %d", testCase.n, testCase.expected, answer)
+		}
+	}
+}
+
+func TestDigitSum(t *testing.T) {
+	testCases := []struct {
+		c        int
+		expected int
+	}{
+		{0, 0},
+		{5, 5},
+		{10, 1},
+		{25, 7},
+		{100000, 1},
+		{100001, 2},
+	}
+
+	for _, testCase := range testCases {
+		answer := DigitSum(testCase.c)
+		if answer != testCase.expected {
+			t.Errorf("ERROR: For %d expected %d, got %d", testCase.c, testCase.expected, answer)
+		}
+	}
+}
+
+func TestHarshad(t *testing.T) {
+	testCases := []struct {
+		c        int
+		expected bool
+	}{
+		{1, true},
+		{2, true},
+		{3, true},
+		{5, true},
+		{7, true},
+		{201, true},
+		{2011, false},
+		{100000, true},
+		{100001, false},
+	}
+
+	for _, testCase := range testCases {
+		answer := Harshad(testCase.c, DigitSum(testCase.c))
+		if answer != testCase.expected {
+			t.Errorf("ERROR: For %d expected %t, got %t", testCase.c, testCase.expected, answer)
+		}
+	}
+}
+
+func TestTotient(t *testing.T) {
+	testCases := []struct {
+		n        int
+		expected int
+	}{
+		{2, 1}, // Totient() has had a lot of bugs. Use lots of test cases!
+		{3, 2},
+		{4, 2},
+		{5, 4},
+		{6, 2},
+		{7, 6},
+		{8, 4},
+		{9, 6},
+		{10, 4},
+		{11, 10},
+		{12, 4},
+		{13, 12},
+		{14, 6},
+		{15, 8},
+		{16, 8},
+		{17, 16},
+		{18, 6},
+		{19, 18},
+		{20, 8},
+		{21, 12},
+		{22, 10},
+		{23, 22},
+		{24, 8},
+		{25, 20},
+		{26, 12},
+		{27, 18},
+		{28, 12},
+		{29, 28},
+		{30, 8},
+		{80, 32},
+		{81, 54},
+		{82, 40},
+		{83, 82},
+		{84, 24},
+		{85, 64},
+		{86, 42},
+		{87, 56},
+		{88, 40},
+		{89, 88},
+		{90, 24},
+		{91, 72},
+		{92, 44},
+		{93, 60},
+		{94, 46},
+		{95, 72},
+		{96, 32},
+		{97, 96},
+		{98, 42},
+		{99, 60},
+	}
+
+	for _, testCase := range testCases {
+		answer := Totient(testCase.n)
+		if answer != testCase.expected {
+			t.Errorf("ERROR: For %d expected %d, got %d", testCase.n, testCase.expected, answer)
+		}
+	}
+}
+
+func TestSquareFree(t *testing.T) {
+	testCases := []struct {
+		c        int
+		expected bool
+	}{
+		{0, true},
+		{1, true},
+		{2, true},
+		{3, true},
+		{4, false},
+		{5, true},
+		{6, true},
+		{7, true},
+		{8, false},
+		{9, false},
+		{10, true},
+		{25, false},
+		{100000, false},
+		{100001, true},
+	}
+
+	for _, testCase := range testCases {
+		answer := SquareFree(testCase.c)
+		if answer != testCase.expected {
+			t.Errorf("ERROR: For %d expected %t, got %t", testCase.c, testCase.expected, answer)
+		}
+	}
+}
+
+func TestPascalTriangle(t *testing.T) {
+	testCases := []struct {
+		row      int
+		col      int
+		expected int
+	}{
+		{0, 0, 1},
+		{1, 0, 1},
+		{1, 1, 1},
+		{2, 0, 1},
+		{2, 1, 2},
+		{2, 2, 1},
+		{3, 0, 1},
+		{3, 1, 3},
+		{3, 2, 3},
+		{3, 3, 1},
+	}
+
+	triangle := PascalTriangle(4)
+
+	for _, testCase := range testCases {
+		answer := triangle[testCase.row][testCase.col]
+		if answer != testCase.expected {
+			t.Errorf("ERROR: For %d %d expected %d, got %d", testCase.row, testCase.col, testCase.expected, answer)
 		}
 	}
 }
