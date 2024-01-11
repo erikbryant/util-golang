bf48f5765e3e6721ecbd9dd6b3b1a63f6dd8da3e
Erik Bryant <erikbryantology@gmail.com>
2024-01-10 22:15:31 -0500

    Make debug message more robust

--- system/system.go
+++ system/system.go
@@ -6,6 +6,7 @@ import (
 	"os/signal"
 	"path"
 	"runtime"
+	"strings"
 	"syscall"
 )
 
@@ -19,17 +20,42 @@ func MyPath(file string) string {
 	return path.Join(dir, file)
 }
 
-// CtrlT prints a debugging message when SIGUSR1 is sent to this process.
-func CtrlT(str string, val *int, digits []int) {
+// dumpDebug prints a debugging message to stderr
+func dumpDebug() {
+	nGo := runtime.NumGoroutine()
+	buf := make([]byte, nGo*1024)
+	runtime.Stack(buf, true)
+	lines := strings.Split(string(buf), "\n")
+	for i := 0; i < len(lines); i++ {
+		if strings.HasPrefix(lines[i], "goroutine ") {
+			fmt.Fprintf(os.Stderr, "%16s", lines[i][9:len(lines[i])-1])
+			if i+1 < len(lines) {
+				fmt.Fprintf(os.Stderr, " %25s", lines[i+1])
+				i++
+			}
+			if i+1 < len(lines) {
+				fmt.Fprintf(os.Stderr, "  %s", lines[i+1])
+				i++
+			}
+			fmt.Fprintf(os.Stderr, "\n")
+		}
+	}
+}
+
+// InstallDebug installs a signal handler that prints a debugging message to stderr when SIGUSR1 is received
+func InstallDebug() int {
+	// To signal a debugging dump:
+	//   $ kill -SIGUSR1 <pid>
+
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, syscall.SIGUSR1)
-
-	fmt.Println("$ kill -SIGUSR1", os.Getpid())
 
 	go func() {
 		for {
 			<-c
-			fmt.Println("^T] ", str, *val, digits)
+			dumpDebug()
 		}
 	}()
+
+	return os.Getpid()
 }
