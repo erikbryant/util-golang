f286fc5869b87c0956ebb64fce0ecd0e6c118ac8
Erik Bryant <erikbryantology@gmail.com>
2025-09-17 12:50:54 +0100

    Add Triangular, Pentagonal, and Hexagonal fcns
    Those functions allow negative parameters and generate valid values.
    Update the Is* tests to include those.

--- figurate/figurate.go
+++ figurate/figurate.go
@@ -9,6 +9,11 @@ import (
 	"github.com/erikbryant/util-golang/algebra"
 )
 
+// Triangular returns the nth triangular number
+func Triangular(n int) int {
+	return n * (n + 1) / 2
+}
+
 // IsTriangular returns true if n is a triangular number
 func IsTriangular(n int) bool {
 	// https://en.wikipedia.org/wiki/Triangular_number
@@ -16,16 +21,32 @@ func IsTriangular(n int) bool {
 	return algebra.IsInt(root)
 }
 
+// Pentagonal returns the nth pentagonal number
+func Pentagonal(n int) int {
+	return n * (3*n - 1) / 2
+}
+
 // IsPentagonal returns true if n is a pentagonal number
 func IsPentagonal(n int) bool {
 	// https://en.wikipedia.org/wiki/Pentagonal_number
+	if n == 0 {
+		return false
+	}
 	root := math.Sqrt(float64(24*n + 1))
-	return algebra.IsInt((root + 1) / 6)
+	return algebra.IsInt((root+1)/6) || algebra.IsInt((-root+1)/6)
+}
+
+// Hexagonal returns the nth hexagonal number
+func Hexagonal(n int) int {
+	return n * (2*n - 1)
 }
 
 // IsHexagonal returns true if n is a hexagonal number
 func IsHexagonal(n int) bool {
 	// https://en.wikipedia.org/wiki/Hexagonal_number
+	if n == 0 {
+		return false
+	}
 	root := math.Sqrt(float64(8*n + 1))
-	return algebra.IsInt((root + 1) / 4)
+	return algebra.IsInt((root+1)/4) || algebra.IsInt((-root+1)/4)
 }
--- figurate/figurate_test.go
+++ figurate/figurate_test.go
@@ -2,6 +2,34 @@ package figurate
 
 import "testing"
 
+func TestTriangular(t *testing.T) {
+	testCases := []struct {
+		n        int
+		expected int
+	}{
+		{0, 0},
+		{1, 1},
+		{2, 3},
+		{3, 6},
+		{4, 10},
+		{5, 15},
+		{6, 21},
+		{-1, 0},
+		{-2, 1},
+		{-3, 3},
+		{-4, 6},
+		{-5, 10},
+		{-6, 15},
+	}
+
+	for _, testCase := range testCases {
+		answer := Triangular(testCase.n)
+		if answer != testCase.expected {
+			t.Errorf("ERROR: For %d expected %d, got %d", testCase.n, testCase.expected, answer)
+		}
+	}
+}
+
 func TestIsTriangular(t *testing.T) {
 	testCases := []struct {
 		c        int
@@ -28,6 +56,34 @@ func TestIsTriangular(t *testing.T) {
 	}
 }
 
+func TestPentagonal(t *testing.T) {
+	testCases := []struct {
+		n        int
+		expected int
+	}{
+		{0, 0},
+		{1, 1},
+		{2, 5},
+		{3, 12},
+		{4, 22},
+		{5, 35},
+		{6, 51},
+		{-1, 2},
+		{-2, 7},
+		{-3, 15},
+		{-4, 26},
+		{-5, 40},
+		{-6, 57},
+	}
+
+	for _, testCase := range testCases {
+		answer := Pentagonal(testCase.n)
+		if answer != testCase.expected {
+			t.Errorf("ERROR: For %d expected %d, got %d", testCase.n, testCase.expected, answer)
+		}
+	}
+}
+
 func TestIsPentagonal(t *testing.T) {
 	testCases := []struct {
 		c        int
@@ -37,9 +93,15 @@ func TestIsPentagonal(t *testing.T) {
 		{-1, false},
 		{0, false},
 		{1, true},
+		{2, true},
 		{5, true},
+		{7, true},
 		{12, true},
+		{15, true},
 		{22, true},
+		{26, true},
+		{40, true},
+		{57, true},
 		{99, false},
 		{1000, false},
 		{1001, true},
@@ -53,6 +115,34 @@ func TestIsPentagonal(t *testing.T) {
 	}
 }
 
+func TestHexagonal(t *testing.T) {
+	testCases := []struct {
+		n        int
+		expected int
+	}{
+		{0, 0},
+		{1, 1},
+		{2, 6},
+		{3, 15},
+		{4, 28},
+		{5, 45},
+		{6, 66},
+		{-1, 3},
+		{-2, 10},
+		{-3, 21},
+		{-4, 36},
+		{-5, 55},
+		{-6, 78},
+	}
+
+	for _, testCase := range testCases {
+		answer := Hexagonal(testCase.n)
+		if answer != testCase.expected {
+			t.Errorf("ERROR: For %d expected %d, got %d", testCase.n, testCase.expected, answer)
+		}
+	}
+}
+
 func TestIsHexagonal(t *testing.T) {
 	testCases := []struct {
 		c        int
@@ -61,11 +151,17 @@ func TestIsHexagonal(t *testing.T) {
 		{-1, false},
 		{0, false},
 		{1, true},
+		{3, true},
 		{5, false},
 		{6, true},
+		{10, true},
 		{12, false},
 		{15, true},
+		{21, true},
 		{28, true},
+		{36, true},
+		{55, true},
+		{78, true},
 		{99, false},
 		{946, true},
 		{947, false},
